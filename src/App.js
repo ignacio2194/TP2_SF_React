3c0582561273f75baf3bc74ee8df5fecced2bad0
Damian Cazal <damiancazal21@gmail.com>
2022-09-05 20:34:53 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import RickAndMortyCards from "./components/RickAndMortyCards";
+// import RickAndMortyCards from "./components/RickAndMortyCards";
+import Usuarios from "./components/Usuarios";
 import './index.css';
+
 function App() {
   return (
     <div className="App ">
      
-       <RickAndMortyCards/>
+       {/* <RickAndMortyCards /> */}
+       <Usuarios />
       
     </div>
   );
