128221a6dcc544d4d0f7af9e573c3d292553a228
guillescharf <scharf.guillermo@gmail.com>
2022-09-05 20:33:20 -0300

    :rocket: Feat: Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
 import RickAndMortyCards from "./components/RickAndMortyCards";
+import ContactUs from "./components/ContactUs";
+import Home from "./components/Home";
 import './index.css';
 
 function App() {
   return (
     <div className="App px-12  ">
-     
-       <RickAndMortyCards/>
+      <NavBar />
+      <Routes>
+          <Route
+              path="/"
+              element={<Home />}
+          />         
+          <Route
+              path="/Cards/"
+              element={<RickAndMortyCards />}
+          /> 
+          <Route
+              path="/Contact/"
+              element={<ContactUs />}
+          />         
+          <Route
+              path="/*"
+              element={<RickAndMortyCards />}
+          />         
+
+      </Routes>  
       
     </div>
   );
