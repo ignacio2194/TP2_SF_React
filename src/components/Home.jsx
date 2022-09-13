bf2903de31cc324598687d5ede1bfbb39c0265f3
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-09-12 22:36:19 -0300

    tp finalizado

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import Footer from "./Footer";
 const Home = () => {
     return(
-        <div className="bg-container text-white text-center text-lg">
+        <div className=" text-white text-center text-lg">
+            <div className="  bg-container absolute top-[10rem] left-0 right-0  ">
             <h2>Proyecto: Second Check Point</h2>
             <h2>Curso: Skill Factory de React.js</h2>
             <h2>Dictado por: Fabian Tureo para Avalith</h2>
@@ -11,6 +12,10 @@ const Home = () => {
                 <li>Cazal, Damian</li>
                 <li>Scharf, Guillermo</li>
             </ul>
+            </div>
+         
+        
+            
             <Footer/>
         </div>        
     )
